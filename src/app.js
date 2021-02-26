2842d80d6f9f7137392fc05e8d83ff132b7bed4c
pratham31012002 <pratham31012002@gmail.com>
2021-02-27 00:37:18 +0530

    initial website added

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+}
+navSlide();
+let dots          = 4;
+let sliderElem    = document.querySelector('.slider')
+let dotElems      = sliderElem.querySelectorAll('.slider__dot')
+let indicatorElem = sliderElem.querySelector('.slider__indicator')
+
+Array.prototype.forEach.call(dotElems, (dotElem) => {
+		
+	dotElem.addEventListener('click', (e) => {
+
+		let currentPos = parseInt(sliderElem.getAttribute('data-pos'))
+		let newPos     = parseInt(dotElem.getAttribute('data-pos'))
+
+		let newDirection     = (newPos > currentPos ? 'right' : 'left')
+		let currentDirection = (newPos < currentPos ? 'right' : 'left')
+
+		indicatorElem.classList.remove(`slider__indicator--${ currentDirection }`)
+		indicatorElem.classList.add(`slider__indicator--${ newDirection }`)		
+		sliderElem.setAttribute('data-pos', newPos)
+		
+	})
+	
+})
