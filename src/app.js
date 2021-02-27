2a41a6766d593256d97741568475609adadaa391
pratham31012002 <pratham31012002@gmail.com>
2021-02-27 13:33:14 +0530

    feat: Added initial website

--- src/app.js
+++ src/app.js
@@ -1,31 +1,26 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    burger.addEventListener('click', ()=>{
-        nav.classList.toggle('nav-active');
-        burger.classList.toggle('toggle');
-    });
+  const burger = document.querySelector('.burger')
+  const nav = document.querySelector('.nav-links')
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active')
+    burger.classList.toggle('toggle')
+  })
 }
-navSlide();
-let dots          = 4;
-let sliderElem    = document.querySelector('.slider')
-let dotElems      = sliderElem.querySelectorAll('.slider__dot')
-let indicatorElem = sliderElem.querySelector('.slider__indicator')
+navSlide()
+const sliderElem = document.querySelector('.slider')
+const dotElems = sliderElem.querySelectorAll('.slider__dot')
+const indicatorElem = sliderElem.querySelector('.slider__indicator')
 
 Array.prototype.forEach.call(dotElems, (dotElem) => {
-		
-	dotElem.addEventListener('click', (e) => {
+  dotElem.addEventListener('click', (e) => {
+    const currentPos = parseInt(sliderElem.getAttribute('data-pos'))
+    const newPos = parseInt(dotElem.getAttribute('data-pos'))
 
-		let currentPos = parseInt(sliderElem.getAttribute('data-pos'))
-		let newPos     = parseInt(dotElem.getAttribute('data-pos'))
+    const newDirection = (newPos > currentPos ? 'right' : 'left')
+    const currentDirection = (newPos < currentPos ? 'right' : 'left')
 
-		let newDirection     = (newPos > currentPos ? 'right' : 'left')
-		let currentDirection = (newPos < currentPos ? 'right' : 'left')
-
-		indicatorElem.classList.remove(`slider__indicator--${ currentDirection }`)
-		indicatorElem.classList.add(`slider__indicator--${ newDirection }`)		
-		sliderElem.setAttribute('data-pos', newPos)
-		
-	})
-	
+    indicatorElem.classList.remove(`slider__indicator--${currentDirection}`)
+    indicatorElem.classList.add(`slider__indicator--${newDirection}`)
+    sliderElem.setAttribute('data-pos', newPos)
+  })
 })
